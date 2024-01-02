b4d3b96a28497c9a4aced02a685b956888da4962
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-01-01 18:07:12 -0800

    Add hello and greetings (first tutorial)

---
+++ greetings/greetings.go
@@ -0,0 +1,37 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf(randFmt(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func randFmt() string {
+	formats := []string{
+		"Hi, %v.",
+		"Welcome, %v, to Wonderland!",
+		"Yolo, %v!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
